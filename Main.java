25e6dd9fd23c5440469f1d00fa9f927e9cb7e1ab
JL Gardiola <88923726+jlgardiola18@users.noreply.github.com>
2024-10-06 07:43:53 +0000

    chapter1part3done

--- Main.java
+++ Main.java
@@ -1,17 +1,18 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         
-        Scanner scanner = new Scanner(System.in);
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("What is your name? ");
-        String name = scanner.nextLine();
+        String name = sc.nextLine();
         System.out.println("How old are you? ");
-        int age = scanner.nextInt();
-        scanner.nextLine();
+        int age = sc.nextInt();
+        sc.nextLine();
         System.out.println("What is your hobby? ");
-        String hobby = scanner.nextLine();
+        String hobby = sc.nextLine();
 
 
         System.out.println("Hello "+name);
