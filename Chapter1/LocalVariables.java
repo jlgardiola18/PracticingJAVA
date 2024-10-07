bc838c68b0ab665d18398ec854e342e3d6757407
JL Gardiola <88923726+jlgardiola18@users.noreply.github.com>
2024-10-07 07:07:55 +0000

    wala pa

---
+++ Chapter1/LocalVariables.java
@@ -0,0 +1,15 @@
+package Chapter1;
+
+public class LocalVariables {
+    public static void main(String[] args) {
+        
+    }
+     public int valid(){
+        int y = 10;
+        int x; // x is declared here
+        x = 3;
+        int reply = x + y;
+        return reply;
+    }
+}
+
