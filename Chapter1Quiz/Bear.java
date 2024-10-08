83f0d7968640856007c12df728f2d17c7b7e1ae4
JL Gardiola <88923726+jlgardiola18@users.noreply.github.com>
2024-10-08 07:06:45 +0000

    chapter1quizdone

---
+++ Chapter1Quiz/Bear.java
@@ -0,0 +1,11 @@
+package Chapter1Quiz;
+
+public class Bear {
+    protected void finalize() {
+        System.out.println("Roar!");
+  }
+  public static void main(String[] args) {
+       Bear bear = new Bear();
+       bear = null;
+       System.gc();
+  } }
