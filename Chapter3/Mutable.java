0d0e8a92dd0996c0a25aafedb16574b45409e547
JL Gardiola <88923726+jlgardiola18@users.noreply.github.com>
2024-10-16 11:25:37 +0000

    chapter3

---
+++ Chapter3/Mutable.java
@@ -0,0 +1,12 @@
+package Chapter3;
+
+public class Mutable {
+    private String s;
+    public void setS(String newS){ s = newS; }
+    public String getS() { return s; }
+}
+
+final class Immutable{
+    private String s = "name";
+    public String getS() { return s; }
+}
